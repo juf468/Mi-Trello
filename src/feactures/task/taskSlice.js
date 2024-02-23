65e0dd47a7958394fd9b92ce308a436f3e17346f
juf468 <julia.franchi7@gmail.com>
2024-02-23 20:23:50 -0300

    first commit

---
+++ src/feactures/task/taskSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+ export const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState:[],
+    reducers:{
+
+    }
+
+})
+export default tasksSlice.reducer
