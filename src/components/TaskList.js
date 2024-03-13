6af11a7434fc0c76a2ce3fa9d3b896de19f623eb
juf468 <julia.franchi7@gmail.com>
2024-03-13 19:35:51 -0300

    taskList styles finished

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -16,21 +16,32 @@ const TaskList = () =>{
    }
 
     return (
-        <div>
-            <header>
+        <div className="w-4/6">
+            <header className="flex justify-between items-center py-4"> 
                 <h1> Task {tasks.length} </h1>
-                <Link to='/create-task'>
-                 Create task
-                </Link>
+                <Link to='/create-task' className="bg-indigo-800 rounded-md px-2 py-1 "> Create task </Link>
             </header>
+            <div className="grid grid-cols-3 gap-4">
         {tasks.map(task => (
-            <div key={task.id}>
+            <div key={task.id} className="bg-neutral-800 p-4 rounded-md">
+                <header className="flex justify-between">
                 <h3>{task.title}</h3>
+                <div className="flex gap-x-2">
+                <Link to={`/edit-task/${task.id}`}
+                className="bg-zinc-600 px-2 py-1 text-xs rounded-md"> Edit </Link>
+                 <button onClick={()=>handleDelete(task.id)}
+                 className="bg-red-400 px-2 py-1 text-xs rounded-md self-center"
+                 >Delete</button> 
+                 
+                </div>
+              
+             </header>
                 <p>{task.description}</p>
-                <button onClick={()=>handleDelete(task.id)}>Delete</button>
-                <Link to={`/edit-task/${task.id}`}> Edit </Link>
+                
+              
             </div>            
         ))}
+        </div>
     </div>
     )
 }
